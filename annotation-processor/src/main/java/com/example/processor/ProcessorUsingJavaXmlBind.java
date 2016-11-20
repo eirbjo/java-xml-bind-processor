f9c271c6e61546423f3a6c4b96a3c775925fa542
Eirik Bjørsnøs <eirbjo@gmail.com>
2016-11-20 22:14:37 +0100

    Print some module debug  info during processing

--- annotation-processor/src/main/java/com/example/processor/ProcessorUsingJavaXmlBind.java
+++ annotation-processor/src/main/java/com/example/processor/ProcessorUsingJavaXmlBind.java
@@ -7,7 +7,7 @@ import javax.annotation.processing.SupportedSourceVersion;
 import javax.lang.model.SourceVersion;
 import javax.lang.model.element.TypeElement;
 import javax.xml.bind.JAXBContext;
-import javax.xml.bind.JAXBException;
+import java.lang.reflect.Layer;
 import java.util.Set;
 
 
@@ -17,6 +17,9 @@ public class ProcessorUsingJavaXmlBind extends AbstractProcessor{
 
     @Override
     public boolean process(Set<? extends TypeElement> set, RoundEnvironment roundEnvironment) {
+
+        System.out.println("java.xml? " +Layer.boot().findModule("java.xml").isPresent());
+        System.out.println("java.xml.bind? " +Layer.boot().findModule("java.xml.bind").isPresent());
 
         System.out.println(JAXBContext.class);
 
